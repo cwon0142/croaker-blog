5b4e22ba9c3a964527ab76a4e9998b5ffbda6cde
cwon0142 <claudia8wong@gmail.com>
2025-10-03 00:42:58 +1000

    Added files

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from config import app
 from flask import render_template
 
+# Load home webpage into browser
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Main method
+if __name__ == '__main__':
+    app.run()
