b5b8af26becbfaedcd732bdf6c3089790b26977d
cwon0142 <claudia8wong@gmail.com>
2025-10-03 00:54:50 +1000

    Added html files

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ from flask import render_template
 # Load home webpage into browser
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home/index.html')
+
+@app.route('/registration')
+def registration():
+    return render_template('accounts/registration.html')
 
 # Main method
 if __name__ == '__main__':
