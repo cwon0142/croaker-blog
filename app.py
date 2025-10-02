3dac610f7bb5bf09615180198110e3b65733375e
cwon0142 <claudia8wong@gmail.com>
2025-10-03 00:31:49 +1000

    Added files

---
+++ app.py
@@ -0,0 +1,3 @@
+from config import app
+from flask import render_template
+
---
+++ config.py
@@ -0,0 +1,4 @@
+from flask import Flask
+
+# Creates an instance of the blog application and defines any configuration details for it
+app = Flask(__name__)
